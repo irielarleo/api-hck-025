6d07172519e4077a2e499afde3f787080deb6471
irielarleo <irelaureleo@gmail.com>
2025-02-04 15:49:04 +0700

    update

--- main.py
+++ main.py
@@ -33,16 +33,11 @@ def search_data(id:int):
 #http://127.0.0.1:8000/data/3
 #http://127.0.0.1:8000/data/3 >>>>> buat test
 
-#menambahkan data
-#@app.post("/data/{add}")
-#def add_data(new_data:string):
-#    new_data = new_data.split('_')
-#   new_row = {'id':new_data[0],
-#              'nama':new_data[1],
-#              'age':new_data[2],
-#             'job':new_data[3]}
+@app.post("/data/add")
+def add_data(new_data:dict):
+    global data
     
-#  new_row = pd.DataFrame(new_data)
-#data = pd.concat([data, new_row], ignore_index=True)
+    new_row = pd.DataFrame([new_data])
+    data = pd.concat([data, new_row], ignore_index=True)
 
-#return{'message': 'data is update !'}
+    return {'message':data.to_dict(orient='records')}
